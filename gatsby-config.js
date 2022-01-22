7329b67cc58645bdb6ea1d4b2b808c9162d36b75
zwj <zwj865738120@163.com>
2022-01-22 22:06:28 +0800

    feat:开发

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,42 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: 'code_xia',
+    siteUrl: `https://personal.ddnszwj.top`,
+  },
+  plugins: [
+    `gatsby-plugin-gatsby-cloud`,
+    // {
+    //   resolve: `gatsby-plugin-create-client-paths`,
+    //   options: { prefixes: [`/app/*`] },
+    // },
+    // "gatsby-plugin-image",
+    // "gatsby-plugin-sharp",
+    "gatsby-plugin-sass",
+    // 'gatsby-plugin-mantine',
+    // "gatsby-transformer-yaml",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `data`,
+    //     path: `${__dirname}/config`,
+    //     ignore: [`**/\.*`], // ignore files starting with a dot
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
     },
-    plugins: [
-
-    ]
-}
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/ // See below to configure properly
+        }
+      }
+    },
+  ],
+};
